c76489bb870e4a337fbd520f234e7252b6dd2a06
ahmedkamaleldeen <ahmedkamaleldeen96@gmail.com>
2022-02-19 23:45:34 +0200

    new updates in portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardlistComponent } from './cardlist/cardlist.component';
+import { RightComponent } from './right/right.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarditemComponent } from './cardlist/carditem/carditem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardlistComponent,
+    RightComponent,
+    FooterComponent,
+    CarditemComponent
   ],
   imports: [
     BrowserModule
